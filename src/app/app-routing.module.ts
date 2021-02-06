8f12d189a1c241fcff0babd9098950deca4c75e0
ngbontsi <qcMXaBxYkzrebvfA5knU>
2021-02-06 15:01:41 +0200

    adding tenants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home';
 import { AuthGuard } from './_helpers';
 import { DashComponent } from './dash/dash.component';
+import {AddTenantComponent} from '@app/add-tenant/add-tenant/add-tenant.component';
+import {SearchTenantComponent} from '@app/search-tenant/search-tenant/search-tenant.component';
 
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
 const usersModule = () => import('./users/users.module').then(x => x.UsersModule);
@@ -11,6 +13,8 @@ const usersModule = () => import('./users/users.module').then(x => x.UsersModule
 const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'dashboard', component: DashComponent, canActivate: [AuthGuard] },
+  { path: 'add-tenant', component: AddTenantComponent, canActivate: [AuthGuard] },
+  { path: 'search-tenant', component: SearchTenantComponent, canActivate: [AuthGuard] },
     { path: 'users', loadChildren: usersModule, canActivate: [AuthGuard] },
     { path: 'account', loadChildren: accountModule },
 
--- src/app/search-tenant/search-tenant/search-tenant.component.ts
+++ src/app/search-tenant/search-tenant/search-tenant.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-search-tenant',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchTenantComponent implements OnInit {
 
-  constructor() { }
+  dynamicForm: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-  }
 
+    this.dynamicForm = this.formBuilder.group({
+      numberOfTickets: ['', Validators.required],
+      tickets: new FormArray([])
+    });
+  }
+  get f() { return this.dynamicForm.controls; }
+  get t() { return this.f.tickets as FormArray; }
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.dynamicForm.invalid) {
+      return;
+    }
+
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.dynamicForm.value, null, 4));
+  }
+  onChangeTickets(e) {
+    const numberOfTickets = e.target.value || 0;
+    if (this.t.length < numberOfTickets) {
+      for (let i = this.t.length; i < numberOfTickets; i++) {
+        this.t.push(this.formBuilder.group({
+          name: ['', Validators.required],
+          email: ['', [Validators.required, Validators.email]]
+        }));
+      }
+    } else {
+      for (let i = this.t.length; i >= numberOfTickets; i--) {
+        this.t.removeAt(i);
+      }
+    }
+  }
+  onReset() {
+    // reset whole form back to initial state
+    this.submitted = false;
+    this.dynamicForm.reset();
+    this.t.clear();
+  }
+  onClear() {
+    // clear errors and reset ticket fields
+    this.submitted = false;
+    this.t.reset();
+  }
 }
