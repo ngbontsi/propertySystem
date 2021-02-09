960f7fb31c6e87c38205faf608edafbc7c18602c
ngbontsi <qcMXaBxYkzrebvfA5knU>
2021-02-09 12:26:39 +0200

    change dashboard icons

--- src/app/store-summary/store-summary.service.ts
+++ src/app/store-summary/store-summary.service.ts
@@ -10,13 +10,13 @@ export class StoreSummaryService {
   getStoreSummary(): Observable<StoreSummary[]> {
     return of([
       // tslint:disable-next-line:max-line-length
-      { title: 'Total Sales', value: '9465', isIncrease: true, color: 'primary', percentValue: '0.5383', icon: 'payments', isCurrency: true },
+      { title: 'Total Rentals', value: '9465', isIncrease: true, color: 'primary', percentValue: '0.5383', icon: 'payments', isCurrency: true },
       // tslint:disable-next-line:max-line-length
-      { title: 'Average Order Value', value: '465', isIncrease: false, color: 'accent', percentValue: '0.2544', icon: 'local_atm', isCurrency: true },
+      { title: 'Average Tenants', value: '465', isIncrease: false, color: 'accent', percentValue: '0.2544', icon: 'bed', isCurrency: true },
       // tslint:disable-next-line:max-line-length
-      { title: 'Total Orders', value: '243', isIncrease: true, color: 'warn', percentValue: '0.4565', icon: 'shopping_cart', isCurrency: false },
+      { title: 'Total Rentals', value: '243', isIncrease: true, color: 'warn', percentValue: '0.4565', icon: 'store-alt', isCurrency: false },
       // tslint:disable-next-line:max-line-length
-      { title: 'Returning Customers', value: '35', isIncrease: false, color: 'primary', percentValue: '0.8361', icon: 'portrait', isCurrency: false }
+      { title: 'Returning Tenants', value: '35', isIncrease: false, color: 'primary', percentValue: '0.8361', icon: 'portrait', isCurrency: false }
     ]);
   }
 
